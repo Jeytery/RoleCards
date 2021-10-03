c3d624a111d8319797651cc533a6409d4591530f
Jeytery <dimaostapchenko@gmail.com>
2021-10-03 21:20:25 +0300

    UserManager: remake clear

--- RoleCards/Model/UserManager.swift
+++ RoleCards/Model/UserManager.swift
@@ -8,9 +8,84 @@
 import Foundation
 
 protocol UserManagerDelegate: AnyObject {
-    
+    func userManagerDidAutorize()
+    func userManager(didGet user: User)
+    func userManagerDidNotGetUser()
+}
+
+extension UserManagerDelegate {
+    func userManagerDidAutorize() {}
 }
 
 class UserManager {
-    static let current = UserManager()
+        
+    static let shared = UserManager()
+    
+    weak var delegate: UserManagerDelegate?
+    
+    private(set) var user: User?
+    private(set) var isAutorizing = false
+    
+    private let id: String = "UserManager.user.id"
+    private let userDefaults = UserDefaults.standard
+    
+    private init() {}
+}
+
+//MARK: - internal funcs
+extension UserManager {
+    func saveUser(_ user: User) {
+        guard let data = try? JSONEncoder().encode(user) else {
+            print("saveUser: unavailable to encode user")
+            return
+        }
+        userDefaults.set(data, forKey: id)
+    }
+    
+    func getUser() -> User? {
+        guard let data = userDefaults.data(forKey: id),
+              let user = try? JSONDecoder().decode(User.self, from: data)
+        else { return nil }
+        return user
+    }
+    
+    private func clearUser() {
+        userDefaults.removePersistentDomain(forName: id)
+    }
+}
+
+//MARK: - public
+extension UserManager {
+    func configure() {
+        isAutorizing = true
+        guard let _user = getUser() else {
+            delegate?.userManagerDidNotGetUser()
+            delegate?.userManagerDidAutorize()
+            isAutorizing = false
+            return
+        }
+        
+        checkIsUserValid(token: _user.token, completion: {
+            [unowned self] result in
+            isAutorizing = false
+            delegate?.userManagerDidAutorize()
+            
+            switch result {
+            case .success(let status):
+                print(status)
+                guard status else {
+                    clearUser()
+                    delegate?.userManagerDidNotGetUser()
+                    return
+                }
+                delegate?.userManager(didGet: _user)
+                user = _user
+                break
+            case .failure(let error):
+                delegate?.userManagerDidNotGetUser()
+                print(error)
+                break
+            }
+        })
+    }
 }
