2fe12f6754260bc350746fe59a75d29cb6d31fd3
Jeytery <dimaostapchenko@gmail.com>
2021-10-05 21:06:23 +0300

    RoleVC: worked version

--- RoleCards/ViewControllers/RoleViewController.swift
+++ RoleCards/ViewControllers/RoleViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Pikko
 
 protocol RoleViewControllerDelegate: AnyObject {
     func roleViewControllerShouldIconForRedButton() -> UIImage
@@ -13,26 +14,83 @@ protocol RoleViewControllerDelegate: AnyObject {
     func roleViewController(didReturn role: Role)
 }
 
+extension RoleViewControllerDelegate {
+    func roleViewControllerShouldIconForRedButton() -> UIImage { return Icons.exit }
+    func roleViewControllerRedButtonDidTap() {}
+}
+
 class RoleViewController: UIViewController {
     
     weak var delegate: RoleViewControllerDelegate?
     
     private let list = StackListView(axis: .vertical)
+    private var nameTextField: DescTextFieldView!
+    private var descTextField: DescTextFieldView!
+    private let colorView = UIView()
     
-    init() {
+    private var listBottomConstraint: NSLayoutConstraint!
+    private var role: Role
+    
+    init(role: Role = Role(name: "", color: Colors.red.roleColor, description: "")) {
+        self.role = role
         super.init(nibName: nil, bundle: nil)
         configureUI()
         configureList()
         configureButtons()
+        fetchData(role)
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardDidHide),
+            name: UIResponder.keyboardDidHideNotification,
+            object: nil)
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        view.endEditing(true)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+}
+
+//MARK: - internal functions
+extension RoleViewController {
+    private func checkReadness() -> Bool {
+        if nameTextField.text == "" {
+            nameTextField.setError(text: "Put role name, please")
+            return false
+        }
+        return true
+    }
+    
+    private func fetchData(_ role: Role) {
+        nameTextField.text = role.name
+        descTextField.text = role.description
+        colorView.backgroundColor = role.color.uiColor
     }
 }
 
 //MARK: - ui
 extension RoleViewController {
+    @objc func keyboardWillShow(_ notification: Notification) {
+        var constant = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue.height
+        constant *= -1
+        listBottomConstraint.constant = constant
+        list.updateConstraints()
+    }
+    
+    @objc func keyboardDidHide(_ notification: Notification) {
+        listBottomConstraint.constant = 0
+        list.updateConstraints()
+    }
+    
     private func configureUI() {
         view.backgroundColor = Colors.background
         title = "Role"
@@ -41,49 +99,63 @@ extension RoleViewController {
     private func configureList() {
         view.addSubview(list)
         list.translatesAutoresizingMaskIntoConstraints = false
-        list.setTopConstraint(self)
+        list.stackView.spacing = 10
+        list.setTopConstraint(self, constant: 20)
+        listBottomConstraint = list.getBottomConstraint(self)
         list.setSideConstraints(self, constant: 20)
         
-        let nameTextField = DescTextFieldView(title: "Role's name",
-                                              placeholder: "For example: Mafia")
+        //name
+        nameTextField = DescTextFieldView(title: "Role's name",
+                                          placeholder: "For example: Mafia")
         list.addView(nameTextField, size: 100)
         
-        let descTextField = DescTextFieldView(title: "Description",
-                                              placeholder: "For example: Mafia can shot at night")
-        list.addView(descTextField, size: 150)
+        //description
+        descTextField = DescTextFieldView(title: "Description",
+                                          placeholder: "For example: Mafia can shot at night")
+        list.addView(descTextField, size: 100)
+    
+        //colorView
+        let colorTitle = UILabel()
+        colorTitle.text = "Color (Tap to choose)"
+        colorTitle.font = .systemFont(ofSize: 18, weight: .semibold)
+        list.addView(colorTitle, size: 20)
+        
+        colorView.layer.cornerRadius = 10
+        colorView.backgroundColor = role.color.uiColor
+        colorView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(colorViewAction)))
+        list.addView(colorView, size: 60)
+    }
+    
+    @objc func colorViewAction() {
+        let colorVC = ColorPickerViewController()
+        colorVC.didChooseColor = {
+            [unowned self] color in
+            role.color = color.roleColor
+            colorView.backgroundColor = role.color.uiColor
+        }
+        navigationController?.pushViewController(colorVC, animated: true)
     }
     
     private func configureButtons() {
         let stackView = UIStackView()
         view.addSubview(stackView)
         stackView.axis = .horizontal
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.setBottomConstraint(self, constant: -25)
-        stackView.topAnchor.constraint(equalTo: list.bottomAnchor).isActive = true
-        stackView.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        stackView.widthAnchor.constraint(equalToConstant: 150).isActive = true
-        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         stackView.spacing = 10
         stackView.distribution = .fillEqually
         
         let confirmButton = UIButton()
-        let dissmisButton = UIButton()
         confirmButton.setPrimaryStyle(icon: Icons.tick, color: Colors.primary)
         confirmButton.addTarget(self, action: #selector(confirmButtonAction), for: .touchDown)
-        dissmisButton.setPrimaryStyle(icon: Icons.exit, color: Colors.red)
-        dissmisButton.addTarget(self, action: #selector(dissmisButtonAction), for: .touchDown)
         
         stackView.addArrangedSubview(confirmButton)
-        stackView.addArrangedSubview(dissmisButton)
+        list.addView(stackView, size: 60)
     }
     
     @objc func confirmButtonAction() {
-        print("confirmButtonAction")
-    }
-    @objc func dissmisButtonAction() {
-        print("dissmisButtonAction")
+        guard checkReadness() else { return }
+        role.name = nameTextField.text
+        role.description = descTextField.text
         dismiss(animated: true, completion: nil)
-        delegate?.roleViewControllerRedButtonDidTap()
+        delegate?.roleViewController(didReturn: role)
     }
 }
-
